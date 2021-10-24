e6f18d7b8976b2f3d37ee86e69b8865ad6b6326e
barisyild <fb_bcy@hotmail.com>
2021-10-24 21:19:41 +0300

    optimization

--- src/openfl/extensions/animate/utils/MathUtil.hx
+++ src/openfl/extensions/animate/utils/MathUtil.hx
@@ -12,7 +12,7 @@ package openfl.extensions.animate.utils;
 
 class MathUtil {
     /** Moves <code>value</code> into the range between <code>min</code> and <code>max</code>. */
-    public static function clamp(value:Float, min:Float, max:Float):Float
+    public static inline function clamp(value:Float, min:Float, max:Float):Float
     {
         return value < min ? min : (value > max ? max : value);
     }
